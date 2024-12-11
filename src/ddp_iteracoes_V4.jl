5fd3ae3728728762ef6bbed2a9e26cff174623c7
davidAlexander08 <david@RJ-NB-333-32948.ons.org.br>
2024-12-11 00:04:30 -0300

    Update PLUNICO

--- src/ddp_iteracoes_V4.jl
+++ src/ddp_iteracoes_V4.jl
@@ -14,12 +14,6 @@ module Main
         end
     end
 
-    ## METODO PL UNICO
-    
-
-
-
-    #METODO PDD
     
     println("codigo: ", no1.codigo, " codigo_intero: ", no1.index, " nivel: ", no1.periodo , " pai: ", no1.pai)
     printa_nos(no1)
@@ -28,6 +22,128 @@ module Main
     conversao_m3_hm3 = (60*60)/1000000
     global Vi = zeros(length(lista_total_de_nos) ,caso.n_uhes)
     [Vi[1,i] = lista_uhes[i].v0 for i in 1:caso.n_uhes]
+
+    #METODO PL UNICO
+
+
+
+    global m = Model(GLPK.Optimizer)
+    gt_vars = Dict{Tuple{Int, Int}, Vector{JuMP.VariableRef}}()
+    gh_vars = Dict{Tuple{Int, Int}, Vector{JuMP.VariableRef}}()
+    turb_vars = Dict{Tuple{Int, Int}, Vector{JuMP.VariableRef}}()
+    vert_vars = Dict{Tuple{Int, Int}, Vector{JuMP.VariableRef}}()
+    vf_vars = Dict{Tuple{Int, Int}, Vector{JuMP.VariableRef}}()
+    deficit_vars = Dict{Tuple{Int, Int}, JuMP.VariableRef}()
+    ## METODO PL UNICO
+    for est in 1:caso.n_est
+        for i_no in mapa_periodos[est].nos
+            println("i_no: ", i_no.codigo, " est: ", est)
+            gt_vars[(est, i_no.codigo)] = @variable(m, [1:caso.n_term], base_name="gt_$(est)_$(i_no.codigo)")
+            gh_vars[(est, i_no.codigo)] = @variable(m, [1:caso.n_uhes], base_name="gh_$(est)_$(i_no.codigo)")
+            turb_vars[(est, i_no.codigo)] = @variable(m, [1:caso.n_uhes], base_name="turb_$(est)_$(i_no.codigo)")
+            vert_vars[(est, i_no.codigo)] = @variable(m, [1:caso.n_uhes], base_name="vert_$(est)_$(i_no.codigo)")
+            vf_vars[(est, i_no.codigo)] = @variable(m, [1:caso.n_uhes], base_name="vf_$(est)_$(i_no.codigo)")
+            deficit_vars[(est, i_no.codigo)] = @variable(m, base_name="def_$(est)_$(i_no.codigo)")
+        end
+    end
+    @objective( m, Min, sum(lista_utes[i].custo_geracao * gt_vars[(est, i_no.codigo)][i] for est in 1:caso.n_est, i_no in mapa_periodos[est].nos, i in 1:caso.n_term)
+    + sum(0.01 * vert_vars[(est, i_no.codigo)][i] for est in 1:caso.n_est, i_no in mapa_periodos[est].nos, i in 1:caso.n_uhes)+
+    sum(sistema.deficit_cost * deficit_vars[(est, i_no.codigo)] for est in 1:caso.n_est, i_no in mapa_periodos[est].nos))
+    
+    #@constraint(m, balanco, sum(m[:gh][i] for i in 1:caso.n_uhes) + sum(m[:gt][i] for i in 1:caso.n_term) + m[:deficit] == sistema.demanda[est])
+    #@constraint(m, c_hidr[i = 1:caso.n_uhes], Vf[i] + Turb[i] + Vert[i] == Vi[no.codigo,i] + (dat_vaz[(dat_vaz.NOME_UHE .== i) .& (dat_vaz.NO .== no.codigo), "VAZAO"][1])) #linha, colun * converte_m3s_hm3
+
+
+    for est in 1:caso.n_est
+        for i_no in mapa_periodos[est].nos
+            @constraint(  m, sum(gh_vars[(est, i_no.codigo)][i] for i in 1:caso.n_uhes) + sum(gt_vars[(est, i_no.codigo)][i] for i in 1:caso.n_term) + deficit_vars[(est, i_no.codigo)] == sistema.demanda[est]   )
+            @constraint(  m, deficit_vars[(est, i_no.codigo)] >= 0 ) 
+            for term in 1:caso.n_term
+                @constraint(m, gt_vars[(est, i_no.codigo)][term] >= 0)
+                @constraint(m, gt_vars[(est, i_no.codigo)][term] <= lista_utes[term].gtmax) #linha, coluna
+            end
+            
+            for i in 1:caso.n_uhes
+
+                @constraint(  m, gh_vars[(est, i_no.codigo)][i] >= 0 ) 
+                @constraint(  m, turb_vars[(est, i_no.codigo)][i] >= 0 ) 
+                @constraint(  m, vert_vars[(est, i_no.codigo)][i] >= 0 ) 
+                @constraint(  m, vf_vars[(est, i_no.codigo)][i] >= 0 ) 
+
+                @constraint(m, gh_vars[(est, i_no.codigo)][i] == turb_vars[(est, i_no.codigo)][i]) #linha, coluna      /converte_m3s_hm3
+                @constraint(m, turb_vars[(est, i_no.codigo)][i] <= lista_uhes[i].turbmax) #linha, coluna
+
+                inflow = dat_vaz[(dat_vaz.NOME_UHE .== i) .& (dat_vaz.NO .== i_no.codigo), "VAZAO"][1]
+                println("Hydro Plant: ", i, " Stage: ", est, " Node: ", i_no.codigo, " Inflow: ", inflow)
+                
+                if est == 1
+                    @constraint(
+                        m, 
+                        vf_vars[(est, i_no.codigo)][i] + 
+                        turb_vars[(est, i_no.codigo)][i] + 
+                        vert_vars[(est, i_no.codigo)][i] == 
+                        Vi[i_no.codigo, i] + inflow
+                    )
+                else
+                    @constraint(
+                        m, 
+                        vf_vars[(est, i_no.codigo)][i] + 
+                        turb_vars[(est, i_no.codigo)][i] + 
+                        vert_vars[(est, i_no.codigo)][i] == 
+                        vf_vars[(est-1, i_no.pai.codigo)][i] + inflow
+                    )
+                end
+            end
+        end
+    end
+
+
+
+
+    print(m)
+    JuMP.optimize!(m)  
+
+    # Print results
+    for est in 1:caso.n_est
+        for i_no in mapa_periodos[est].nos
+            for i in 1:caso.n_uhes
+                vf = JuMP.value(vf_vars[(est, i_no.codigo)][i])
+                turb = JuMP.value(turb_vars[(est, i_no.codigo)][i])
+                vert = JuMP.value(vert_vars[(est, i_no.codigo)][i])
+                gh = JuMP.value(gh_vars[(est, i_no.codigo)][i])
+                println("Stage: $est, Node: $(i_no.codigo), Hydro Plant: $i, VF: $vf, Turb: $turb, Vert: $vert, Generation: $gh")
+            end
+                
+            for term in 1:caso.n_term
+                gt = JuMP.value(gt_vars[(est, i_no.codigo)][term])
+                println("GT: ", JuMP.value(gt)," est: ", est, " node: ", i_no.codigo)
+            end
+            def = JuMP.value(deficit_vars[(est, i_no.codigo)])
+            println("Deficit = ", def)
+
+        end
+    end
+
+
+    #@variable(m, 0 <= gt[1:caso.n_term])
+    #@variable(m, 0 <= gh[1:caso.n_uhes])
+    #@variable(m, 0 <= Turb[1:caso.n_uhes])
+    #@variable(m, 0 <= Vert[1:caso.n_uhes])
+    #@variable(m, 0 <= Vf[1:caso.n_uhes])
+    #@variable(m, 0 <= deficit )
+    #@variable(m, 0 <= alpha )
+    #@constraint(m, [i = 1:caso.n_uhes], Turb[i] <= lista_uhes[i].turbmax) #linha, coluna
+    #@constraint(m, [i = 1:caso.n_uhes], gh[i] == Turb[i]) #linha, coluna      /converte_m3s_hm3
+    #@constraint(m, [i = 1:caso.n_term], gt[i] <= lista_utes[i].gtmax) #linha, coluna
+    #@objective(m, Min, sum(Vert[i]*0.01 for i in 1:caso.n_uhes) + sum(lista_utes[i].custo_geracao*gt[i] for i in 1:caso.n_term) + sistema.deficit_cost*deficit + alpha)
+    #@constraint(m, balanco, sum(m[:gh][i] for i in 1:caso.n_uhes) + sum(m[:gt][i] for i in 1:caso.n_term) + m[:deficit] == sistema.demanda[est])
+    #@constraint(m, c_hidr[i = 1:caso.n_uhes], Vf[i] + Turb[i] + Vert[i] == Vi[no.codigo,i] + (dat_vaz[(dat_vaz.NOME_UHE .== i) .& (dat_vaz.NO .== no.codigo), "VAZAO"][1])) #linha, colun * converte_m3s_hm3
+
+
+    exit(1)
+
+    #METODO PDD
+
     FCF_coef = zeros(caso.n_est,caso.n_uhes, caso.n_iter)
     FCF_indep = zeros(caso.n_est, caso.n_iter)
     zinf = zeros(caso.n_iter)
